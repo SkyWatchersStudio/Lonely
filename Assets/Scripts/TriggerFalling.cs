6c1ca3ddb43b4be1602f8abf76a5421e647e0aad
Time <erfantime.me@gmail.com>
2019-02-10 15:44:59 +0330

    problem occured

--- Assets/Scripts/TriggerFalling.cs
+++ Assets/Scripts/TriggerFalling.cs
@@ -1,14 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class TriggerFalling : MonoBehaviour
 {
+    public BoxCollider2D[] m_boxColliders;
+    public float m_wait = 2;
+    
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.name == "player")
+        ColliderDesiabler();
+    }
+    void ColliderDesiabler()
+    {
+        foreach (BoxCollider2D box in m_boxColliders)
         {
-            Debug.Log("Player entered...");
+            box.isTrigger = true;
+            Debug.Log($"{box} is trigger.");
+            
+            yield return new WaitForSeconds(m_wait);
         }
     }
 }
