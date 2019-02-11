84e3982654fe9298b8f4f6b184838d7550142fdc
Time <erfantime.me@gmail.com>
2019-02-11 11:18:52 +0330

    still not working

--- Assets/Scripts/TriggerFalling.cs
+++ Assets/Scripts/TriggerFalling.cs
@@ -1,25 +1,30 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 public class TriggerFalling : MonoBehaviour
 {
     public BoxCollider2D[] m_boxColliders;
     public float m_wait = 2;
-    
+
     void OnTriggerEnter2D(Collider2D other)
     {
         ColliderDesiabler();
     }
     void ColliderDesiabler()
     {
+        int i = 1;
         foreach (BoxCollider2D box in m_boxColliders)
         {
-            box.isTrigger = true;
-            Debug.Log($"{box} is trigger.");
+            StartCoroutine(WaitTime());
             
-            yield return new WaitForSeconds(m_wait);
+            box.isTrigger = true;
+            Debug.Log($"box {i} is trigger now.");
+            i++;
         }
+    }
+
+    IEnumerator WaitTime()
+    {
+        yield return new WaitForSeconds(m_wait);
     }
 }
