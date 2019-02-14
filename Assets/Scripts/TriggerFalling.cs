e08331b8f0fe032fa76ef7b760d51ff235e5841f
Time <erfantime.me@gmail.com>
2019-02-14 09:24:06 +0330

    unity auto

--- Assets/Scripts/TriggerFalling.cs
+++ Assets/Scripts/TriggerFalling.cs
@@ -6,9 +6,10 @@ public class TriggerFalling : MonoBehaviour
     public BoxCollider2D[] m_boxColliders;
     public float m_wait = 2;
     private int i = 0;
-
     void OnTriggerEnter2D()
     {
+        //Repeats calling function until cancel invoke.
+        //first m_wait is for first waiting and second m_wait is for every call waiting...
         InvokeRepeating("ColliderDisabler", m_wait, m_wait);
     }
     void ColliderDisabler()
