6bc2f6a64fc684c542cd4fdad5188b29fb79ec59
etzl <erfanzamani3445@gmail.com>
2019-02-16 12:44:37 +0330

    new changes

--- Assets/Scripts/TriggerLettersCollect.cs
+++ Assets/Scripts/TriggerLettersCollect.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class TriggerLettersCollect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private void OnTriggerEnter2D(Collider2D other) {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
