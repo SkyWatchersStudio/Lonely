c3d8a21f3e614119807ef2e874349094c21836b8
etzl <erfanzamani3445@gmail.com>
2019-02-16 14:54:47 +0330

    OnTrigger Working

--- Assets/Scripts/TriggerLettersCollect.cs
+++ Assets/Scripts/TriggerLettersCollect.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class TriggerLettersCollect : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other) {
+    public float xRect = 25;
+    public float yRect = 25;
+    public float widthRect = 100;
+    public float heightRect = 30;
+    public string textField = "Hello, World!";
+    
+    private bool checkEnterance = false;
 
+    private void OnTriggerEnter2D() {
+        checkEnterance = true;
+    }
+    private void OnTriggerExit2D() {
+        checkEnterance = false;
     }
     private void OnGUI() {
+        Rect guiPos;
         
+        if (checkEnterance) {
+            guiPos  = new Rect(xRect, yRect, widthRect, heightRect);
+
+            textField = GUI.TextField (guiPos, textField);
+        }
     }
 }
