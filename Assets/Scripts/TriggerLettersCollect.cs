0bc62767000050e6e3268cc5e34a72418568f0fa
etzl <erfanzamani3445@gmail.com>
2019-02-19 23:09:30 +0330

    comment added

--- Assets/Scripts/TriggerLettersCollect.cs
+++ Assets/Scripts/TriggerLettersCollect.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class TriggerLettersCollect : MonoBehaviour
 {
+    //Positioning the GUI elements
     public Rect[] buttonPos;
     public Rect box;
-    
+
     private float n = 56.42f;
     private string[] letters;
     private bool checkEnterance = false;
     private int clicked, firstClicked;
     private string boxString, value;
 
+    //reset everything on enter trigger again:
     private void OnTriggerEnter2D() {
         letters = new string[4] {"e", "l", "l", "a"};
         clicked = 0;
@@ -20,12 +22,14 @@ public class TriggerLettersCollect : MonoBehaviour
         checkEnterance = true;
     }
     private void OnTriggerExit2D() => checkEnterance = false;
-        
-    private void OnGUI() {        
-        if (checkEnterance) 
+
+    //The actuall representation goes here:
+    private void OnGUI() {
+        if (checkEnterance)
         {
+            //a box in buttons background that shows the complete name
             GUI.Box(box, boxString);
-            
+
             if (GUI.Button(buttonPos[0], letters[0]))
                 OnClick(0);
             if (GUI.Button(buttonPos[1], letters[1]))
@@ -38,19 +42,23 @@ public class TriggerLettersCollect : MonoBehaviour
     }
     private void OnClick(int num)
     {
+        //as we want to track our player click so we can swap to button string...
         clicked++;
         if (!clicked.Equals(2))
         {
+            //store the value of first click and wait untill second clicked happen
             firstClicked = num;
             return;
         }
-        
+
+        //a temporary variable to swap our values
         string tempVar = letters[num];
         letters[num] = letters[firstClicked];
         letters[firstClicked] = tempVar;
 
         clicked = 0;
 
+        //update string that shows in box for complete name:
         boxString = letters[0] + letters[1] + letters[2] + letters[3] + "?";
     }
 }
