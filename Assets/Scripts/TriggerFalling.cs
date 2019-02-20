005cfa76a389efaa5dad7d9f841c989bbb8a0c4e
etzl <erfanzamani3445@gmail.com>
2019-02-20 09:14:19 +0330

    comment added

--- Assets/Scripts/TriggerFalling.cs
+++ Assets/Scripts/TriggerFalling.cs
@@ -7,17 +7,19 @@ public class TriggerFalling : MonoBehaviour
     public float m_wait = 2;
     private int i = 0;
 
-    void OnTriggerEnter2D()
-    {
-        InvokeRepeating("ColliderDisabler", m_wait, m_wait);
-    }
+    //ivoke function call a function after specified time InvokeRepeating call it untill cacelInvoke call
+    void OnTriggerEnter2D() => InvokeRepeating("ColliderDisabler", m_wait, m_wait);
+    
     void ColliderDisabler()
     {
+        //i++ when calculate shows previouse value so i will change after this statement...
         m_boxColliders[i++].enabled = false;
+        //like Debug.Log(), why should we use that instead of print
         print($"{i} is disabled");
 
         if (i == m_boxColliders.Length)
         {
+            //if calcelInvoke get argument it cancels exacly that functin invoke
             CancelInvoke("ColliderDisabler");
         }
     }
