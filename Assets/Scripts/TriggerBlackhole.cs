995c2e346bc697fbcd42c703ae6ce2f7ffb0465f
etzl <erfanzamani3445@gmail.com>
2019-02-22 19:09:28 +0330

    cinematic

--- Assets/Scripts/TriggerBlackhole.cs
+++ Assets/Scripts/TriggerBlackhole.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerBlackhole : MonoBehaviour
-{    
-    public int m_TimeWait = 5;
-
-    private void OnTriggerEnter2D()
-    {
-        Entered = true;
-    }
+{
+    //for cinematic:
+    public float m_TimeWait = 5;
+    private PlayerController playerScript;
 
     public static bool Entered { get; private set;}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        playerScript = other.GetComponent<PlayerController>();
+
+        Entered = true;
+        playerScript.enabled = false;
+    }
+    private void Update()
+    {
+        if (!Entered || playerScript.enabled)
+            return;
+
+        //after waiting 5 second cinematic
+        if (m_TimeWait <= 0)
+            playerScript.enabled = true;
+        m_TimeWait -= Time.deltaTime;
+    }
 }
