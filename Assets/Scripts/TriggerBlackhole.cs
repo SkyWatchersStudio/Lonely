b9a0c58748a1f4937dd63a0c267c8333b132ce13
etzl <erfanzamani3445@gmail.com>
2019-02-22 17:20:02 +0330

    Trigger Added For Rotate

---
+++ Assets/Scripts/TriggerBlackhole.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBlackhole : MonoBehaviour
+{    
+    private void OnTriggerEnter2D() => Entered = true;
+
+    public static bool Entered { get; private set;}
+}
