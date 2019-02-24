61605ad992978659235f17cd63eda02b16c1afeb
etzl <erfanzamani3445@gmail.com>
2019-02-24 12:27:19 +0330

    fixed some issues

--- Assets/Scripts/TriggerBlackhole.cs
+++ Assets/Scripts/TriggerBlackhole.cs
@@ -5,20 +5,24 @@ public class TriggerBlackhole : MonoBehaviour
     //for cinematic:
     public float m_TimeWait = 5;
     private PlayerController playerScript;
+    //Virtual cameras game object...
     public GameObject vCam1, vCam2;
 
     public static bool Entered { get; private set;}
     private void OnTriggerEnter2D(Collider2D other)
     {
         playerScript = other.GetComponent<PlayerController>();
+        //Stop the player for cinematic...
         other.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 
         Entered = true;
         DisablerCam();
         playerScript.enabled = false;
     }
+    //Active and DeActivate vcams:
     void DisablerCam()
     {
+        //if the object is active deactivate it else active it...
         vCam1.SetActive(!vCam1.activeSelf);
         vCam2.SetActive(!vCam2.activeSelf);
     }
