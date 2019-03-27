bac767279a45db57a75acb1e05ca00900c5c5781
etzl <erfanzamani3445@gmail.com>
2019-03-27 19:13:53 +0430

    Optimized Lever

--- Assets/Scripts/TriggerLever.cs
+++ Assets/Scripts/TriggerLever.cs
@@ -3,24 +3,46 @@ using UnityEngine;
 public class TriggerLever : MonoBehaviour
 {
     public float m_Force = 20;
-    
-    private Rigidbody2D m_rigidbody;
+    public Vector2 m_AnchorPoint;
+
     private bool m_trigger = false;
-    private void Awake() 
+    private bool m_secondTrigger = false;
+    private JointAngleLimits2D m_angle;
+    private HingeJoint2D m_joint;
+    private Rigidbody2D m_rigidbody;
+
+    private void Start()
     {
-        m_rigidbody = GetComponent<Rigidbody2D>();
+        //limit angle
+        m_angle.max = 45;
+
+        //Refrences:
+        m_rigidbody = gameObject.AddComponent<Rigidbody2D>();
+        m_joint = gameObject.AddComponent<HingeJoint2D>();
+        //Set joint at desire
+        m_joint.limits = m_angle;
+        m_joint.anchor = m_AnchorPoint;
     }
-    private void Update() 
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
+        {
+            //Trigger the lever
+            m_rigidbody.AddForce(new Vector2(m_Force, 0), ForceMode2D.Impulse);
             m_trigger = true;
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+            m_secondTrigger = true;
     }
-    private void FixedUpdate() 
+    private void FixedUpdate()
     {
         if (m_trigger)
         {
-            m_rigidbody.AddForce(new Vector2(m_Force, 0), ForceMode2D.Impulse);
-            m_trigger = false;
+            if (m_rigidbody.velocity != Vector2.zero)
+                return;
+            Destroy(m_joint);
+            Destroy(m_rigidbody);
+            this.enabled = false;
         }
     }
 }
