3f2cf4ac849f7d6de08db4c335b0bbef27f03dd2
etzl <erfanzamani3445@gmail.com>
2019-03-28 19:43:01 +0430

    New Changes

--- Assets/Scripts/TriggerLever.cs
+++ Assets/Scripts/TriggerLever.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 
 public class TriggerLever : MonoBehaviour
 {
-    public float m_Force = 20;
+    public float m_Force = 30;
     public Transform m_AnchorPoint;
 
     private bool m_trigger = false;
-    private JointAngleLimits2D m_angle;
     private HingeJoint2D m_joint;
     private Rigidbody2D m_rigidbody;
 
@@ -28,13 +27,15 @@ public class TriggerLever : MonoBehaviour
         //Refrences:
         m_rigidbody = gameObject.AddComponent<Rigidbody2D>();
         m_joint = gameObject.AddComponent<HingeJoint2D>();
+        JointAngleLimits2D angle = new JointAngleLimits2D();
 
         //limit angle
-        m_angle.max = 45;
+        angle.max = 45;
         //Set joint at desire
-        m_joint.limits = m_angle;
+        m_joint.limits = angle;
         m_joint.anchor = transform.InverseTransformPoint(m_AnchorPoint.position);
     }
+    //Destroy Components:
     private void OnDisable()
     {
         Destroy(m_joint);
