e89968a26a547f7514d0da78e563046fff6be3fe
etzl <erfanzamani3445@gmail.com>
2019-03-28 20:23:39 +0430

    Trigger Added

--- Assets/Scripts/TriggerLever.cs
+++ Assets/Scripts/TriggerLever.cs
@@ -4,23 +4,41 @@ public class TriggerLever : MonoBehaviour
 {
     public float m_Force = 30;
     public Transform m_AnchorPoint;
+    public BoxCollider2D m_Collider;
 
     private bool m_trigger = false;
     private HingeJoint2D m_joint;
     private Rigidbody2D m_rigidbody;
 
-    private void Update()
+    private void OnDrawGizmos()
     {
-        if (Input.GetButtonDown("Interact"))
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, m_Collider.size);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        OnTriggerEnter2D(other);
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
         {
-            SetValues();
-            //Trigger the lever
-            m_rigidbody.AddForce(new Vector2(m_Force, 0), ForceMode2D.Impulse);
-            m_trigger = true;
+            if (Input.GetButtonDown("Interact"))
+            {
+                SetValues();
+                //Trigger the lever
+                m_rigidbody.AddForce(new Vector2(m_Force, 0), ForceMode2D.Impulse);
+                m_trigger = true;
+            }
+            if (m_trigger)
+                if (m_rigidbody.velocity == Vector2.zero)
+                    this.enabled = false;
         }
-        if (m_trigger)
-            if (m_rigidbody.velocity == Vector2.zero)
-                this.enabled = false;
+    }
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        OnTriggerStay2D(other);
     }
     private void SetValues()
     {
