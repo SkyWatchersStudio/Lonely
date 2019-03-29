97b0393891834acbdee9788fb7ac91b971d5ca4a
etzl <erfanzamani3445@gmail.com>
2019-03-29 14:15:01 +0430

    test with player succesful

---
+++ Assets/Scripts/PlayerMovementTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementTest : MonoBehaviour
+{
+    private void Update() 
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector2(moveInput * Time.deltaTime * 10, 0));
+    }
+}
