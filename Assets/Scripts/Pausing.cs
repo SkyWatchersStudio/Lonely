c06a536625d4fa7f01676971cdd5b68fab835066
etzl <erfanzamani3445@gmail.com>
2019-04-03 23:35:38 +0430

    pause in progress

--- Assets/Scripts/Pausing.cs
+++ Assets/Scripts/Pausing.cs
@@ -2,18 +2,40 @@
 
 public class Pausing : MonoBehaviour
 {
+    private float m_width;
+    private float m_height;
+    private Camera m_mainCamera;
+
+    private void Awake()
+    {
+        m_width = Screen.width;
+        m_height = Screen.height;
+        m_mainCamera = Camera.main;
+    }
     private void OnEnable()
     {
-        transform.position = Camera.main.transform.position;
+        //set position...
+        transform.position = m_mainCamera.transform.position;
         transform.position -= Vector3.forward * -1;
+        
+
+        //test:
+        Vector3 scale = transform.localScale;
+        scale = scale / Screen.width;
+        transform.localScale = scale;
+        
+
+        //change the scale of background pause:
+        transform.localScale += new Vector3(Screen.width / m_width, Screen.height / m_height);
+
+        //back everything to normal
         Time.timeScale = 0;
+        m_width = Screen.width;
+        m_height = Screen.height;
     }
-    private void OnMouseDown()
+    private void OnDisable()
     {
-        Application.Quit();
-    }
-    private void OnDisable() 
-    {
+        //bring frames to normal
         Time.timeScale = 1;
     }
 }
