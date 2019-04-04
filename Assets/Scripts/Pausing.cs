f305b4165f9bbd1b254e18735ff0bda25cfa8b28
etzl <erfanzamani3445@gmail.com>
2019-04-04 12:29:53 +0430

    working on new solution

--- Assets/Scripts/Pausing.cs
+++ Assets/Scripts/Pausing.cs
@@ -2,40 +2,20 @@
 
 public class Pausing : MonoBehaviour
 {
-    private float m_width;
-    private float m_height;
     private Camera m_mainCamera;
-
     private void Awake()
     {
-        m_width = Screen.width;
-        m_height = Screen.height;
         m_mainCamera = Camera.main;
     }
     private void OnEnable()
     {
         //set position...
         transform.position = m_mainCamera.transform.position;
+        //bring sprite in front of camera...
         transform.position -= Vector3.forward * -1;
-        
-
-        //test:
-        Vector3 scale = transform.localScale;
-        scale = scale / Screen.width;
-        transform.localScale = scale;
-        
-
-        //change the scale of background pause:
-        transform.localScale += new Vector3(Screen.width / m_width, Screen.height / m_height);
-
         //back everything to normal
         Time.timeScale = 0;
-        m_width = Screen.width;
-        m_height = Screen.height;
     }
-    private void OnDisable()
-    {
-        //bring frames to normal
-        Time.timeScale = 1;
-    }
+    //bring frames to normal...
+    private void OnDisable() => Time.timeScale = 1;
 }
