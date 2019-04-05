fb8cbf81287edc33ca7fa789e4d3eea45be0d792
etzl <erfanzamani3445@gmail.com>
2019-04-05 12:03:44 +0430

    adding canvas and buttons... etc

--- Assets/Scripts/Pausing.cs
+++ Assets/Scripts/Pausing.cs
@@ -2,19 +2,13 @@
 
 public class Pausing : MonoBehaviour
 {
-    private Camera m_mainCamera;
-    private void Awake()
-    {
-        m_mainCamera = Camera.main;
-    }
     private void OnEnable()
     {
-        //set position...
-        transform.position = m_mainCamera.transform.position;
-        //bring sprite in front of camera...
-        transform.position -= Vector3.forward * -1;
-        //back everything to normal
         Time.timeScale = 0;
+    }
+    private void OnMouseDown() 
+    {
+        Application.Quit();
     }
     //bring frames to normal...
     private void OnDisable() => Time.timeScale = 1;
