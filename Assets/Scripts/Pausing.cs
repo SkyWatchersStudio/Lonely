68f94e8c0fdf670205f3ed19acb091f37eb2b3e3
etzl <erfanzamani3445@gmail.com>
2019-04-06 10:18:15 +0430

    sound effect added to pause

--- Assets/Scripts/Pausing.cs
+++ Assets/Scripts/Pausing.cs
@@ -2,6 +2,11 @@
 
 public class Pausing : MonoBehaviour
 {
+    public AudioSource m_pauseAudioSource;
+    private void OnEnable() 
+    {
+        m_pauseAudioSource.Play();
+    }
     public void QuitTheGame() 
     {
         Application.Quit();
