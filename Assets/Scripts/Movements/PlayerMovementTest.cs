e91e63e74f644512472d6a86837048492cd9241b
etzl <erfanzamani3445@gmail.com>
2019-04-07 18:48:40 +0430

    player now have jump and grounds colliders and other fixed

--- Assets/Scripts/Movements/PlayerMovementTest.cs
+++ Assets/Scripts/Movements/PlayerMovementTest.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 public class PlayerMovementTest : MonoBehaviour
 {
     public float m_MoveSpeed = 10f;
+    public float m_JumpForce = 700;
     public float m_Lerping = .25f;
+    public float m_GroundRadious = .2f;
     public GameObject m_PauseObj;
+    public Transform m_Ground;
+    public LayerMask m_WhatIsGround;
 
     private Rigidbody2D m_rigidbody = null;
     private float m_moveInput = 0f;
     private Vector2 m_moveVel = Vector2.zero;
     private bool m_isPause = false;
+    private bool m_isGround = true;
 
 
     private void Start()
@@ -18,7 +23,13 @@ public class PlayerMovementTest : MonoBehaviour
     }
     private void Update()
     {
-        GetInput();
+        //check the ground...
+        m_isGround = Physics2D.OverlapCircle((Vector2)m_Ground.position, m_GroundRadious, m_WhatIsGround);
+        
+        m_moveInput = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) && m_isGround)
+            Jumping();
+        GetPauseInput();
     }
     private void FixedUpdate()
     {
@@ -26,9 +37,12 @@ public class PlayerMovementTest : MonoBehaviour
     }
 
 
-    void GetInput()
+    void Jumping()
     {
-        m_moveInput = Input.GetAxis("Horizontal");
+        m_rigidbody.AddForce(new Vector2(0, m_JumpForce));
+    }
+    void GetPauseInput()
+    {
         if (Input.GetKeyDown(KeyCode.Escape) && !m_isPause)
         {
             m_isPause = true;
@@ -46,5 +60,11 @@ public class PlayerMovementTest : MonoBehaviour
     {
         m_moveVel = Vector2.Lerp(m_rigidbody.velocity, m_moveInput * Vector2.right * m_MoveSpeed, m_Lerping);
         m_rigidbody.velocity = m_moveVel;
+    }
+
+    private void OnDrawGizmos() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(m_Ground.position, m_GroundRadious);
     }
 }
