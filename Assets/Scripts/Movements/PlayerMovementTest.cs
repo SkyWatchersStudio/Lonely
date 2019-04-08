a4812e1b27ce840398e10162d311756a871eaa7f
etzl <erfanzamani3445@gmail.com>
2019-04-08 19:47:28 +0430

    deleted player jump and everything bringed back to normal

--- Assets/Scripts/Movements/PlayerMovementTest.cs
+++ Assets/Scripts/Movements/PlayerMovementTest.cs
@@ -3,18 +3,13 @@ using UnityEngine;
 public class PlayerMovementTest : MonoBehaviour
 {
     public float m_MoveSpeed = 10f;
-    public float m_JumpForce = 700;
     public float m_Lerping = .25f;
-    public float m_GroundRadious = .2f;
     public GameObject m_PauseObj;
-    public Transform m_Ground;
-    public LayerMask m_WhatIsGround;
 
     private Rigidbody2D m_rigidbody = null;
     private float m_moveInput = 0f;
-    private Vector2 m_moveVel = Vector2.zero;
+    private float m_xVelocity = 0;
     private bool m_isPause = false;
-    private bool m_isGround = true;
 
 
     private void Start()
@@ -23,12 +18,7 @@ public class PlayerMovementTest : MonoBehaviour
     }
     private void Update()
     {
-        //check the ground...
-        m_isGround = Physics2D.OverlapCircle((Vector2)m_Ground.position, m_GroundRadious, m_WhatIsGround);
-        
         m_moveInput = Input.GetAxis("Horizontal");
-        if (Input.GetKeyDown(KeyCode.Space) && m_isGround)
-            m_rigidbody.AddForce(new Vector2(0, m_JumpForce));
         GetPauseInput();
     }
     private void FixedUpdate()
@@ -52,12 +42,7 @@ public class PlayerMovementTest : MonoBehaviour
     }
     void Moving()
     {
-        m_moveVel = Vector2.Lerp(m_rigidbody.velocity, m_moveInput * Vector2.right * m_MoveSpeed, m_Lerping);
-        m_rigidbody.velocity = m_moveVel;
-    }
-    private void OnDrawGizmos() 
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(m_Ground.position, m_GroundRadious);
+        m_xVelocity = Mathf.Lerp(m_rigidbody.velocity.x, m_moveInput * m_MoveSpeed, m_Lerping);
+        m_rigidbody.velocity = new Vector2(m_xVelocity, m_rigidbody.velocity.y);
     }
 }
